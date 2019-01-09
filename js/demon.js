d1738260cd33148a070328b389f6fb9eba32491c
Andrew Harper <drewmharp@gmail.com>
2019-01-09 18:25:00 -0500

    (Mostly) Functional Animations & ShapeKeys
    The first animation out of three, along with the ShapeKeys of a demo 
    file (sample-anim.glb) are functional. Still need to get other 
    animations to display properly.

--- js/demon.js
+++ js/demon.js
@@ -1,147 +1,224 @@
 
-  if ( WEBGL.isWebGLAvailable() === false ) {
-    document.body.appendChild( WEBGL.getWebGLErrorMessage() );
-  }
+			if ( WEBGL.isWebGLAvailable() === false ) {
 
-  var container, stats, clock, gui, mixer, actions, activeAction, previousAction;
-  var camera, scene, renderer, model, face;
-  var api = { state: 'Walk' };
-  init();
-  animate();
-  function init() {
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
-    camera.position.set( - 50, 50, 100 );
-    camera.lookAt( new THREE.Vector3( 0, 2, 0 ) );
+				document.body.appendChild( WEBGL.getWebGLErrorMessage() );
 
-    camControls = new THREE.OrbitControls(camera);
-    camControls.autoRotate = true;
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xe0e0e0 );
-    scene.fog = new THREE.Fog( 0xe0e0e0, 20, 100 );
-    clock = new THREE.Clock();
-    // lights
-    var light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    light.position.set( 0, 20, 0 );
-    scene.add( light );
-    light = new THREE.DirectionalLight( 0xffffff );
-    light.position.set( 0, 20, 10 );
-    scene.add( light );
-    // ground
-    var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-    mesh.rotation.x = - Math.PI / 2;
-    scene.add( mesh );
-    var grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
-    grid.material.opacity = 0.2;
-    grid.material.transparent = true;
-    scene.add( grid );
-    // model
-    var loader = new THREE.GLTFLoader();
-    loader.load( '../resources/demon-from-json.glb', function( gltf ) {
-      model = gltf.scene;
-      //Make double sided (remove later)
-      model.traverse(
-        function (node) {
-          if (node.material){
-            node.material.side = THREE.DoubleSide;
-          }
-        }
-      );
-      scene.add( model );
-      createGUI( model, gltf.animations );
-    }, undefined, function( e ) {
-      console.error( e );
-    } );
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.gammaOutput = true;
-    renderer.gammaFactor = 2.2;
-    container.appendChild( renderer.domElement );
-    window.addEventListener( 'resize', onWindowResize, false );
-    // stats
-    stats = new Stats();
-    container.appendChild( stats.dom );
-  }
-  function createGUI( model, animations ) {
-      var states = [];
-      var emotes = [];
-      gui = new dat.GUI();
-      mixer = new THREE.AnimationMixer( model );
-      actions = {};
-      if (animations.length == 0){
-        gui.addFolder('No animations')
-      } else {
-        for ( var i = 0; i < animations.length; i++ ) {
-          var clip = animations[ i ];
-          var action = mixer.clipAction( clip );
-          actions[ clip.name ] = action;
-          states.push(clip.name);
-        }
+			}
 
+			var container, stats, clock, gui, mixer, actions, activeAction, previousAction;
+			var camera, scene, renderer, model, face;
 
-      // states
-      var statesFolder = gui.addFolder( 'States' );
-      var clipCtrl = statesFolder.add( api, 'state' ).options( states );
-      clipCtrl.onChange( function() {
-        fadeToAction( api.state, 0.5 );
-      } );
-      statesFolder.open();
-      // emotes
-      var emoteFolder = gui.addFolder( 'Emotes' );
-      function createEmoteCallback( name ) {
-        api[ name ] = function() {
-          fadeToAction( name, 0.2 );
-          mixer.addEventListener( 'finished', restoreState );
-        };
-        emoteFolder.add( api, name );
-      }
-      }
-      function restoreState() {
-        mixer.removeEventListener( 'finished', restoreState );
-        fadeToAction( api.state, 0.2 );
-      }
-      for ( var i = 0; i < emotes.length; i++ ) {
-        createEmoteCallback( emotes[ i ] );
-      }
-      emoteFolder.open();
-      // expressions
-      face = model.getObjectByName( 'Head_2' );
-      var expressions = Object.keys( face.morphTargetDictionary );
-      var expressionFolder = gui.addFolder('Expressions');
-      for ( var i = 0; i < expressions.length; i++ ) {
-        expressionFolder.add( face.morphTargetInfluences, i, 0, 1 ).name( expressions[ i ] );
-      }
-      activeAction = actions['Walk'];
-      activeAction.play();
-      expressionFolder.open();
-  }
-  function fadeToAction( name, duration ) {
-    previousAction = activeAction;
-    activeAction = actions[ name ];
-    if ( previousAction !== activeAction ) {
-      previousAction.fadeOut( duration );
-    }
-    activeAction
-      .reset()
-      .setEffectiveTimeScale( 1 )
-      .setEffectiveWeight( 1 )
-      .fadeIn( duration )
-      .play();
-  }
-  function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-  }
-  //
-  function animate() {
-    var dt = clock.getDelta();
-    if ( mixer ) mixer.update( dt );
+			var api = { state: 'FirstAnim' };
 
-    requestAnimationFrame( animate );
+			init();
+			animate();
 
-    renderer.render( scene, camera );
-    stats.update();
-  }
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
+				camera.position.set( - 5, 3, 10 );
+				camera.lookAt( new THREE.Vector3( 0, 2, 0 ) );
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xe0e0e0 );
+				scene.fog = new THREE.Fog( 0xe0e0e0, 20, 100 );
+
+				clock = new THREE.Clock();
+
+				// lights
+
+				var light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+				light.position.set( 0, 20, 0 );
+				scene.add( light );
+
+				light = new THREE.DirectionalLight( 0xffffff );
+				light.position.set( 0, 20, 10 );
+				scene.add( light );
+
+				// ground
+
+				var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+				mesh.rotation.x = - Math.PI / 2;
+				scene.add( mesh );
+
+				var grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
+				grid.material.opacity = 0.2;
+				grid.material.transparent = true;
+				scene.add( grid );
+
+				// model
+
+				var loader = new THREE.GLTFLoader();
+				loader.load( 'resources/sample-anim.glb', function( gltf ) {
+
+					model = gltf.scene;
+					scene.add( model );
+
+					createGUI( model, gltf.animations );
+
+				}, function (e) {
+          console.error(e);
+        }, function( e ) {
+
+					console.error( e );
+
+				} );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.gammaOutput = true;
+				renderer.gammaFactor = 2.2;
+				container.appendChild( renderer.domElement );
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+				// stats
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+			}
+
+			function createGUI( model, animations ) {
+
+				var states = [ ];
+				var emotes = [ ];
+
+				gui = new dat.GUI();
+
+				mixer = new THREE.AnimationMixer( model );
+
+				actions = {};
+
+				for ( var i = 0; i < animations.length; i++ ) {
+
+					var clip = animations[ i ];
+					var action = mixer.clipAction( clip );
+					actions[ clip.name ] = action;
+        /*
+					if ( emotes.indexOf( clip.name ) >= 0 || states.indexOf( clip.name ) >= 4 ) {
+
+							action.clampWhenFinished = true;
+							action.loop = THREE.LoopOnce;
+
+					}
+        */
+        states.push(clip.name);
+
+				}
+
+				// states
+
+				var statesFolder = gui.addFolder( 'States' );
+
+				var clipCtrl = statesFolder.add( api, 'state' ).options( states );
+
+				clipCtrl.onChange( function() {
+
+					fadeToAction( api.state, 0.5 );
+
+				} );
+
+				statesFolder.open();
+
+				// emotes
+
+				var emoteFolder = gui.addFolder( 'Emotes' );
+
+				function createEmoteCallback( name ) {
+
+					api[ name ] = function() {
+
+						fadeToAction( name, 0.2 );
+
+						mixer.addEventListener( 'finished', restoreState );
+
+					};
+
+					emoteFolder.add( api, name );
+
+				}
+
+				function restoreState() {
+
+					mixer.removeEventListener( 'finished', restoreState );
+
+					fadeToAction( api.state, 0.2 );
+
+				}
+
+				for ( var i = 0; i < emotes.length; i++ ) {
+
+					createEmoteCallback( emotes[ i ] );
+
+				}
+
+				emoteFolder.open();
+
+				// expressions
+
+				cube = model.getObjectByName( 'Cube' );
+
+				var expressions = Object.keys( cube.morphTargetDictionary );
+				var expressionFolder = gui.addFolder('Expressions');
+
+				for ( var i = 0; i < expressions.length; i++ ) {
+
+					expressionFolder.add( cube.morphTargetInfluences, i, 0, 1 ).name( expressions[ i ] );
+
+				}
+
+				activeAction = actions['FirstAnim'];
+				activeAction.play();
+
+				expressionFolder.open();
+
+			}
+
+			function fadeToAction( name, duration ) {
+
+				previousAction = activeAction;
+				activeAction = actions[ name ];
+
+				if ( previousAction !== activeAction ) {
+
+					previousAction.fadeOut( duration );
+
+				}
+
+				activeAction
+					.reset()
+					.setEffectiveTimeScale( 1 )
+					.setEffectiveWeight( 1 )
+					.fadeIn( duration )
+					.play();
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			//
+
+			function animate() {
+
+				var dt = clock.getDelta();
+
+				if ( mixer ) mixer.update( dt );
+
+				requestAnimationFrame( animate );
+
+				renderer.render( scene, camera );
+
+				stats.update();
+
+			}
